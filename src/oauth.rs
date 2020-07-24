249af8325d43a87f59661b96ab7ecdae95141218
Luis Hebendanz <luis.nixos@gmail.com>
2020-07-24 12:08:39 +0200

    Working oauth

--- src/oauth.rs
+++ src/oauth.rs
@@ -1,35 +1,48 @@
-use actix::{Actor, Addr, Arbiter, Context, System};
+use actix::{Actor, Context};
 use goauth::auth::JwtClaims;
+use std::time::Duration;
 use goauth::scopes::Scope;
 use goauth::credentials::Credentials;
-use goauth::GoErr;
 use goauth::get_token_as_string_legacy;
 use smpl_jwt::Jwt;
 use log::{error, debug};
+use actix::prelude::AsyncContext;
 
+const SECS_IN_MINUTE: u64 = 60;
 pub struct MyActor;
+
+pub fn get_token()-> String{
+    let credentials = Credentials::from_file("/home/lhebendanz/.config/gcloud/tagify-key.json").unwrap();
+    let claims = JwtClaims::new(credentials.iss(),
+                         &Scope::DevStorageReadWrite,
+                         credentials.token_uri(),
+                         None, None);
+    let jwt = Jwt::new(claims, credentials.rsa_key().unwrap(), None);
+    debug!("Calling get_token");
+
+    let token = match get_token_as_string_legacy(&jwt, Some(&credentials.token_uri())){
+        Ok(i) => i,
+        Err(e) => {
+            error!("Error on get_token_as_string: {}",e);
+            std::process::exit(2);
+        }
+    };
+    return token;
+}
 
 impl Actor for MyActor {
     type Context = Context<Self>;
 
+
     fn started(&mut self, ctx: &mut Self::Context) {
 
-        let credentials = Credentials::from_file("/home/lhebendanz/.config/gcloud/tagify-key.json").unwrap();
-        let claims = JwtClaims::new(credentials.iss(),
-                             &Scope::DevStorageReadWrite,
-                             credentials.token_uri(),
-                             None, None);
-        let jwt = Jwt::new(claims, credentials.rsa_key().unwrap(), None);
-        debug!("I am alive!");
+        let mut token = get_token();
+        debug!("Token is {}", token);
 
-        let token = match get_token_as_string_legacy(&jwt, Some(&credentials.token_uri())){
-            Ok(i) => i,
-            Err(e) => {
-                error!("Error on get_token_as_string: {}",e);
-                std::process::exit(2);
-            }
-        };
-        println!("Token is {}", token);
-        System::current().stop(); // <- stop system
+        ctx.run_interval(Duration::new(15*SECS_IN_MINUTE, 0), move |_act, _ctx| {
+            token = get_token();
+            debug!("Token is {}", token);
+        });
+
     }
 }
